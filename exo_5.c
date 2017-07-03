f0828ffb77bd840f6513b32302cb0f5cde41d5ab
Akatheris <maximousse27@gmail.com>
2017-07-03 17:41:17 +0200

    myputstr avec 1 seul appel de write

---
+++ exo_5.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main (void) 
+{
+
+	char chaine[]="Salut";
+	myputstrww(chaine);
+
+
+
+
+return(0);
+}
+
+void myputstrww (char* chaine)
+{
+	int i = my_strlen(chaine);
+	int y = 0 ;
+	write(1,&chaine[y],i);
+	
+}
+#include <stdio.h>
+#include <stdlib.h>
+int my_strlen(const char* chaine)
+{
+	int i = 0 ;
+	while (chaine[i] != '\0')
+	{
+	i++;
+ 	}
+  	return i;
+ }
